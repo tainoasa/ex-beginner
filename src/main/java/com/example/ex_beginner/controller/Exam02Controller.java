7550cee06b79623f3b939b10839c40df6bb00f80
tainoasa <tainoasa@gmail.com>
2026-05-19 14:23:25 +0900

    exam01完了

--- src/main/java/com/example/ex_beginner/controller/Exam02Controller.java
+++ src/main/java/com/example/ex_beginner/controller/Exam02Controller.java
@@ -1,5 +1,7 @@
 package com.example.ex_beginner.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +15,19 @@ public class Exam02Controller {
         return "exam02";
     }
 
+    @Autowired
+    private HttpSession session;
+
     @PostMapping("/show-result")
     public String showResult(int num1, int num2) {
+        session.setAttribute("num1", num1);
+        session.setAttribute("num2", num2);
+        session.setAttribute("calcResult", num1 + num2);
         return "exam02-result";
+    }
+
+    @GetMapping("/show-result2")
+    public String showResult2() {
+        return "exam02-result2";
     }
 }
