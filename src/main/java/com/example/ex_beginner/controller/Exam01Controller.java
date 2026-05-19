2f4993047bf2f5896763e2ac621f2223aa96ec21
tainoasa <tainoasa@gmail.com>
2026-05-19 13:56:43 +0900

    exam01完了

---
+++ src/main/java/com/example/ex_beginner/controller/Exam01Controller.java
@@ -0,0 +1,22 @@
+package com.example.ex_beginner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam01")
+public class Exam01Controller {
+    @GetMapping()
+    public String index() {
+        return "exam01";
+    }
+
+    @PostMapping("/show-name")
+    public String showName(String name, Model model) {
+        model.addAttribute("name", name);
+        return "exam01-result";
+    }
+}
